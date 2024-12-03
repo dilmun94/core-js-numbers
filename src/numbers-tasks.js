d8c24a4df3f8b654ea2bef5597f647cb7a712244
Anna Nitsevich <dilmun94@gmail.com>
2024-12-03 19:32:38 +0500

    feat: add task 295 string

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -292,8 +292,13 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  let sum = 0;
+  const n = String(num);
+  for (let i = 0; i < n.length; i += 1) {
+    sum += Number(n[i]);
+  }
+  return sum;
 }
 
 /**
